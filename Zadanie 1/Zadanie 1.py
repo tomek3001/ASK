827f8082faffe0b590f4c9ca6b10cd79b584314d
Tomasz Szymański <5520639-tszyma@users.noreply.gitlab.com>
2020-03-19 12:00:31 +0100

    Update files

---
+++ Zadanie 1/Zadanie 1.py
@@ -0,0 +1,15 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.button import Label
+kivy.require('1.11.1')
+
+class Zadanie1App(App):
+    def build(self):
+        return Label()
+
+
+
+Zadanie1 = Zadanie1App()
+
+Zadanie1.run()
