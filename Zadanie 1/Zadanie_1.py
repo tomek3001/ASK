4063a886bcfe4d68f2156536cc4039c2d1988665
Jakub Osielski <s171973@student.pg.edu.pl>
2020-03-30 22:01:08 +0200

    Add functionality to Registers - add MOV

--- Zadanie 1/Zadanie_1.py
+++ Zadanie 1/Zadanie_1.py
@@ -5,15 +5,19 @@ from kivy.uix.button import Label
 kivy.require('1.11.1')
 from Register import Register
 
+
 class Zadanie1App(App):
     def build(self):
         return Label()
 
+
 Zadanie1 = Zadanie1App()
 
-Zadanie1.run()
+#Zadanie1.run()
 
-AX = Register(0b00000000)
-BX = Register(0b00010000)
-CX = Register(0b00100000)
-DX = Register(0b00110000)
+AX = Register()
+BX = Register()
+CX = Register()
+DX = Register()
+
+print(ord('C'))
