d7873d373796f589521e22fef1937d7a2de77e6e
Tomasz Szymański <5520639-tszyma@users.noreply.gitlab.com>
2020-04-02 15:49:42 +0200

    Variables (registers' content) are now instantly displayed in GUI

--- Zadanie 1/C#/MainWindow.xaml.cs
+++ Zadanie 1/C#/MainWindow.xaml.cs
@@ -26,21 +26,157 @@ namespace zadanie1
 
         int new_command_length = 0;
 
+        public void registersReset()
+        {
+
+            this.DataContext = this;
+            this.AXVal = 0;
+            this.AHVal = 0;
+            this.ALVal = 0;
+            this.BXVal = 0;
+            this.BHVal = 0;
+            this.BLVal = 0;
+            this.CXVal = 0;
+            this.CHVal = 0;
+            this.CLVal = 0;
+            this.DXVal = 0;
+            this.DHVal = 0;
+            this.DLVal = 0;
+        }
+
+        //*************************************GLOBAL REGISTER VARIABLES**************************************
+        public int AXVal
+        {
+            get { return (int)GetValue(AXValProperty); }
+            set { SetValue(AXValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty AXValProperty =
+            DependencyProperty.Register("AXVal", typeof(int), typeof(MainWindow));
+
+        public int AHVal
+        {
+            get { return (int)GetValue(AHValProperty); }
+            set { SetValue(AHValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty AHValProperty =
+            DependencyProperty.Register("AHVal", typeof(int), typeof(MainWindow));
+
+
         public int ALVal
         {
-            get { return (int)GetValue(ALValProperty);  }
-            set { SetValue(ALValProperty, value);  }
+            get { return (int)GetValue(ALValProperty); }
+            set { SetValue(ALValProperty, value); }
 
         }
 
         public static readonly DependencyProperty ALValProperty =
             DependencyProperty.Register("ALVal", typeof(int), typeof(MainWindow));
 
+
+        public int BXVal
+        {
+            get { return (int)GetValue(BXValProperty); }
+            set { SetValue(BXValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty BXValProperty =
+            DependencyProperty.Register("BXVal", typeof(int), typeof(MainWindow));
+
+        public int BHVal
+        {
+            get { return (int)GetValue(BHValProperty); }
+            set { SetValue(BHValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty BHValProperty =
+            DependencyProperty.Register("BHVal", typeof(int), typeof(MainWindow));
+
+        public int BLVal
+        {
+            get { return (int)GetValue(BLValProperty); }
+            set { SetValue(BLValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty BLValProperty =
+            DependencyProperty.Register("BLVal", typeof(int), typeof(MainWindow));
+
+        public int CXVal
+        {
+            get { return (int)GetValue(CXValProperty); }
+            set { SetValue(CXValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty CXValProperty =
+            DependencyProperty.Register("CXVal", typeof(int), typeof(MainWindow));
+
+        public int CHVal
+        {
+            get { return (int)GetValue(CHValProperty); }
+            set { SetValue(CHValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty CHValProperty =
+            DependencyProperty.Register("CHVal", typeof(int), typeof(MainWindow));
+
+        public int CLVal
+        {
+            get { return (int)GetValue(CLValProperty); }
+            set { SetValue(CLValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty CLValProperty =
+            DependencyProperty.Register("CLVal", typeof(int), typeof(MainWindow));
+
+        public int DXVal
+        {
+            get { return (int)GetValue(DXValProperty); }
+            set { SetValue(DXValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty DXValProperty =
+            DependencyProperty.Register("DXVal", typeof(int), typeof(MainWindow));
+
+        public int DHVal
+        {
+            get { return (int)GetValue(DHValProperty); }
+            set { SetValue(DHValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty DHValProperty =
+            DependencyProperty.Register("DHVal", typeof(int), typeof(MainWindow));
+
+        public int DLVal
+        {
+            get { return (int)GetValue(DLValProperty); }
+            set { SetValue(DLValProperty, value); }
+
+        }
+
+        public static readonly DependencyProperty DLValProperty =
+            DependencyProperty.Register("DLVal", typeof(int), typeof(MainWindow));
+
+        //****************************************************************************************************
+
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = this;
-            this.ALVal = 0;
+            registersReset();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -77,17 +213,8 @@ namespace zadanie1
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
-            //string przycisk = "";
-            //if (ADDButton.IsChecked == true)
-            //    przycisk = "ADD";
-            //else if (SUBButton.IsChecked == true)
-            //    przycisk = "SUB";
-            //else if (MOVButton.IsChecked == true)
-            //    przycisk = "MOV";
-            //OutputTextBox.Text = "Wciśnięty został przycisk CLEAR, a wybrana operacja to " + przycisk +
-            //    ", podczas gdy argument pierwszy to " + ARG1Val.Text + ", a argument drugi to " + ARG2Val.Text + ".";
-
             OutputTextBox.Text = "";
+            registersReset();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -109,7 +236,18 @@ namespace zadanie1
             OutputTextBox.Text = OutputTextBox.Text + przycisk + " " + ARG1Val.Text + "," + ARG2Val.Text + "\n";
             new_command_length = OutputTextBox.Text.Length - original_length;
             this.DataContext = this;
-            this.ALVal = 110;
+            this.AXVal += 1;
+            this.AHVal += 2;
+            this.ALVal += 3;
+            this.BXVal += 4;
+            this.BHVal += 5;
+            this.BLVal += 6;
+            this.CXVal += 7;
+            this.CHVal += 8;
+            this.CLVal += 9;
+            this.DXVal += 10;
+            this.DHVal += 11;
+            this.DLVal += 12;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
