cc17be6b6bfa03c0f41c55e3e3bd093c586dee66
Jakub Osielski <s171973@student.pg.edu.pl>
2020-05-30 19:16:30 +0200

    Trzy pierwsze zadania - 2 w pełni działające, trzecie w przygotowaniu.

--- ASK_5.py
+++ ASK_5.py
@@ -3,19 +3,18 @@ import sys
 from ctypes import Structure
 import time
 import keyboard
-
-from PySide2.QtWidgets import QGraphicsPixmapItem
 from numpy.random import randint as rand
+from random import random
 import threading
-from concurrent.futures import ThreadPoolExecutor
-
+import pygame
 import resources
 
 # Class containing all displayed subtitles
 class TEXT(Structure):
 
     Invitation = "Hello, we are grateful that you wanted to take part in our psychomotor test." \
-                 " There are 4 tasks ahead of you that will allow us to test your skills. Good luck!"
+                 " There are 4 tasks ahead of you that will allow us to test your skills. Good luck!" \
+                 "\nFirst you have to click button as fast as possible"
     window_title = "Psychomotor tests"
     start_button = "Press to start"
 
@@ -23,7 +22,9 @@ class TEXT(Structure):
     tab_2 = "Results"
 
     first_task = "Press buttons as fast as possible"
-
+    second_task = "Your next task is to capture the sound as fast as possible wait for sound three times." \
+                  "Press space if you heard sound"
+    third_task = "Your third task is to  check all  squares."
     click_me = "Click me!"
 
     your_score = "Your score in this test is: "
@@ -32,9 +33,18 @@ class TEXT(Structure):
 
     end = "Thanks you for participation"
 
+    file_name = "smashing.wav"
+
+    hex = ":/hex.png"
+    circle = ":/circle.png"
+    square = ":/square.png"
+    oktagon = ":/oktagon.png"
+
+    shapes = [hex, circle, square, oktagon]
+
 
 class PARAMS(Structure):
-    test_1_duration = 1    # time in secs
+    test_1_duration = 2    # time in secs
     interval = 50000
 
 
@@ -50,6 +60,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphicsScene = QtWidgets.QGraphicsScene(self.tab)
         self.graphicsView = QtWidgets.QGraphicsView(self.graphicsScene, self)
         self.graphicsView.setGeometry(0, 300, self.width(), self.height())
+        self.items = []
+        for item in range(1):
+            self.items.append(QtWidgets.QGraphicsPixmapItem())
+            self.items[-1].setPixmap(QtGui.QPixmap(":/hex.png")) #TEXT.shapes[rand(0,4)]))
+            self.items[-1].setPos(50 , 50) #rand(0, self.graphicsView.width()), rand(0, self.graphicsView.height()))
+            self.items[-1].setScale(0.2)
+            #self.items[-1].setRotation(random())
+            self.items[-1].setVisible(True)
+
         self.startButton = QtWidgets.QPushButton(self.tab)
         self.startButton.setGeometry(QtCore.QRect(360, 350, 200, 51))
         font = QtGui.QFont()
@@ -72,8 +91,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.tab_2 = QtWidgets.QWidget()
         self.tabWidget.addTab(self.tab_2, TEXT.tab_2)
         self.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(self)
-        self.setStatusBar(self.statusbar)
         self.tabWidget.setTabEnabled(1, False)
         self.tabWidget.setCurrentIndex(0)
         # Continue button
@@ -95,27 +112,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.firstTaskButton.setVisible(False)
         self.pointCounter = 0
         self.testNumber = 0
-        self.globalCursorPos = (-1, -1)
-
-
+        self.stop = 0
         # Second task button
-        self.secondTaskButton = QtWidgets.QPushButton(self.tab)
-        self.secondTaskButton.setGeometry(QtCore.QRect(360, 350, 200, 51))
-        font = QtGui.QFont()
-        font.setPointSize(15)
-        self.secondTaskButton.setFont(font)
-        self.secondTaskButton.setText(TEXT.click_me)
-        self.secondTaskButton.setVisible(False)
+        self.reaction_time = 0
 
         self.graphicsView.setVisible(False)
         self.startButton.clicked.connect(self.startTest)
         self.firstTaskButton.clicked.connect(self.addPoint)
         self.continueButton.clicked.connect(self.tasksCounter)
-
-    def mousePressEvent(self, event):
-        self.globalCursorPos = (event.pos().x(), event.pos().y())
-        time.sleep(0.5)
-        self.globalCursorPos = (-1, -1)
 
     def startTest(self):
         start = time.time()
@@ -132,16 +136,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.testNumber += 1
         self.continueButton.setVisible(False)
         if self.testNumber == 1:
-            self.graphicsView.setVisible(True)
+            self.label.setText(TEXT.second_task)
+            self.thread = threading.Thread(target=self.makeSound)
+            self.thread.start()
+
         if self.testNumber == 2:                 # TO TRZEBA ZMIENIĆ NIE KOŃCZYMY PO DWÓCH ZADANIACH
+            self.label.setText(TEXT.third_task)
+            self.graphicsView.setVisible(True)
+        if self.testNumber == 3:
             self.endProgram()
 
     def keyPressEvent(self, event):
         key = event.key()
         if key == 32:
-            pass
-
-
+            self.stop = time.time()
 
     def endProgram(self):
         self.label.setText(TEXT.end)
@@ -165,10 +173,20 @@ class MainWindow(QtWidgets.QMainWindow):
         return rand(0, self.width() - 200), rand(0, self.height() - 102)
 
     # Second test
-
-
-
-
+    def makeSound(self):
+        self.continueButton.setVisible(False)
+        for i in range(1):
+            time.sleep(rand(2, 6))
+            start = time.time()
+            pygame.mixer.init()
+            pygame.mixer_music.load(TEXT.file_name)
+            pygame.mixer_music.play()
+            while self.stop - start < 0:
+                pass
+            self.reaction_time += time.time() - start       # [BŁĄD] COŚ TU Z SUMĄ NIE GRA
+        self.continueButton.setVisible(True)
+        self.label.setText(TEXT.your_score + str(self.reaction_time) + " s")
+        self.continueButton.setVisible(True)
 
 
 if __name__ == '__main__':
